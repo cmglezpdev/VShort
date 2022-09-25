5c28a3eb006188a2cfddc43e8381957a4670ded6
Carlos Manuel Gonzalez <cmglezpdev@gmail.com>
2022-09-24 21:49:26 -0400

    feat(Root): add a new background image value in the tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'player': "url('/assets/play.png')"
+      } 
+    },
   },
   plugins: [],
 }

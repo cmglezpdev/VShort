f2197403796a29617de88095d60ff1ea1023f690
Carlos Manuel Gonzalez <cmglezpdev@gmail.com>
2022-09-25 01:42:28 -0400

    fix(Root): fix the snap scroll in the main

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
 import { FeedVideos } from "./components"
 
-FeedVideos
-
-
 function App() {
 
   return (
     <div className='bg-black grid place-content-center'>
-        <main className='w-screen snap-y snap-mandatory overflow-y-scroll'>
+        <main className='w-screen snap-y snap-mandatory overflow-y-scroll h-screen'>
           <FeedVideos />
         </main>
     </div>

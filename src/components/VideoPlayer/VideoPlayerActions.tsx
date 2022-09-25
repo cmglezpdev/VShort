24aef0e3dcf28306424e2ec0aaf4c7f5c6f7ea38
Carlos Manuel Gonzalez <cmglezpdev@gmail.com>
2022-09-25 00:13:43 -0400

    feat(Root): add icons to the menu right

--- src/components/VideoPlayer/VideoPlayerActions.tsx
+++ src/components/VideoPlayer/VideoPlayerActions.tsx
@@ -1,18 +1,29 @@
-import { Heart } from "../Icons";
+import { Comments, Heart, Share } from "../Icons";
+import { FC } from 'react';
+
+interface Props {
+    comments?: number,
+    likes?: number,
+    shares?: number,
+    hearted?: boolean
+}
 
 
-export const VideoPlayerActions = () => {
+export const VideoPlayerActions:FC<Props> = ({ comments = 80, likes = 10000, shares = 39, hearted = false }) => {
 
     return (
-        <aside className="absolute bottom-4 right-16">
-            <div className="">
-                <Heart />
+        <aside className="absolute bottom-4 right-4">
+            <div className="text-center">
+                <Heart fill="#fff" />
+                <strong title="like">{likes}</strong>
             </div>
-            <div className="">
-                <Heart fill="red" />
+            <div className="text-center">
+                <Comments fill="#fff" />
+                <strong title="comments">{comments}</strong>
             </div>
-            <div className="">
-                <Heart />
+            <div className="text-center">
+                <Share fill="#fff" />
+                <strong title="share">{shares}</strong>
             </div>
         </aside>
     )

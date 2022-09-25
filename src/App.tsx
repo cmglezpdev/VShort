1ccc93726b35798130c28dc36c7727eb2a8dd60b
Carlos Manuel Gonzalez <cmglezpdev@gmail.com>
2022-09-24 23:50:36 -0400

    refactor(Root): delete innecesary styles

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
 
   return (
     <div className='bg-black grid place-content-center'>
-        <main className='w-screen max-w-[375px] aspect-[9/16] snap-y snap-mandatory overflow-y-scroll'>
+        <main className='w-screen snap-y snap-mandatory overflow-y-scroll'>
           <FeedVideos />
         </main>
     </div>
